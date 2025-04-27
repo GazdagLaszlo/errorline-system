4427e507cacdb5371ba48d317a8f4508c452973a
Machács Roland <machacsroland@icloud.com>
2025-04-27 17:22:32 +0200

    try

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -10,15 +10,15 @@ import {
     UserApi
 } from "../../generated-sources/openapi";
 
-const baseURL = `${import.meta.env.VITE_REST_API_URL}`;
+//const baseURL = `${import.meta.env.VITE_REST_API_URL}`;
 
-const User = new UserApi(new Configuration({basePath: baseURL}), undefined, axiosInstance);
-const Equipment = new EquipmentApi(new Configuration({basePath: baseURL}), undefined, axiosInstance);
-const EquipmentOrder = new EquipmentOrderApi(new Configuration({basePath: baseURL}), undefined, axiosInstance);
-const Facility = new FacilityApi(new Configuration({basePath: baseURL}), undefined, axiosInstance);
-const Issue = new IssueApi(new Configuration({basePath: baseURL}), undefined, axiosInstance);
-const IssueType = new IssueTypeApi(new Configuration({basePath: baseURL}), undefined, axiosInstance);
-const MaintenanceManager = new MaintenanceManagerApi(new Configuration({basePath: baseURL}), undefined, axiosInstance);
+const User = new UserApi(new Configuration({basePath: ' '}), undefined, axiosInstance);
+const Equipment = new EquipmentApi(new Configuration({basePath: ' '}), undefined, axiosInstance);
+const EquipmentOrder = new EquipmentOrderApi(new Configuration({basePath: ' '}), undefined, axiosInstance);
+const Facility = new FacilityApi(new Configuration({basePath: ' '}), undefined, axiosInstance);
+const Issue = new IssueApi(new Configuration({basePath: ' '}), undefined, axiosInstance);
+const IssueType = new IssueTypeApi(new Configuration({basePath: ' '}), undefined, axiosInstance);
+const MaintenanceManager = new MaintenanceManagerApi(new Configuration({basePath: ' '}), undefined, axiosInstance);
 
 const api = {
     User,
